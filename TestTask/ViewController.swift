5c782b4811c0b228589e53c6746995ef6f039add
Dima <bazhanuk17@gmail.com>
2020-05-17 15:10:02 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Dima Bazhaniuk on 17.05.2020.
+//  Copyright © 2020 Dima Bazhaniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
